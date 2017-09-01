b30798a947a848f678a0d7800da0644008b3d2bf
Kieron Richardson <artdude543@hotmail.co.uk>
2017-09-01 16:37:27 +0100

    feat(pretzel): add support for pretzel song getting.

--- src/methods/index.ts
+++ src/methods/index.ts
@@ -1,20 +1,26 @@
-import * as stream from './channel';
+import * as channel from './channel';
 import * as str from './string';
 import * as users from './users';
 
 import { exoapi } from './exoapi';
 import { lastfm } from './lastfm';
 import { mixer } from './mixer';
+import { pretzel } from './pretzel';
 import { smashcast } from './smashcast';
+import { time } from './time';
 import { twitch } from './twitch';
+import { weather } from './weather';
 
 export const methods = {
     exoapi,
     lastfm,
     mixer,
+    pretzel,
     smashcast,
+    time,
     twitch,
-    ...stream,
+    weather,
+    ...channel,
     ...str,
     ...users,
 };
---
+++ tests/methods/pretzel.ts
@@ -0,0 +1,39 @@
+import { test } from 'ava';
+import { memoize } from 'decko';
+import * as fetch from 'isomorphic-fetch';
+import { cloneDeep } from 'lodash';
+
+import { pretzel } from '../../src/methods/pretzel';
+import { mockMessage, mockSettings } from '../../src/mock';
+
+const regexTest = /Now Playing: (.*) by (.*) -> (.*)/i;
+
+test.beforeEach(t => {
+    const message = cloneDeep(mockMessage);
+    message.provider.type = 'mixer';
+    t.context = { request: (<any> memoize)(fetch), message };
+});
+
+test('handle an invalid channel', async t => {
+    t.is(await pretzel(t.context.message, mockSettings, t.context.request, 'all', 'uioh8y98797'), '[No Song Playing or Channel Not Found]');
+});
+
+test('parse song', async t => {
+    t.context.message.channel.name = 'Chikachi';
+    t.regex(await pretzel(t.context.message, mockSettings, t.context.request, 'song'), /([\w]*)\w+/i);
+});
+
+test('parse artist', async t => {
+    t.context.message.channel.name = 'Chikachi';
+    t.regex(await pretzel(t.context.message, mockSettings, t.context.request, 'artist'), /([\w]*)\w+/i);
+});
+
+test('parse link', async t => {
+    t.context.message.channel.name = 'Chikachi';
+    t.regex(await pretzel(t.context.message, mockSettings, t.context.request, 'link'), /([\w]*)\w+/i);
+});
+
+test('parse default', async t => {
+    t.context.message.channel.name = 'Chikachi';
+    t.regex(await pretzel(t.context.message, mockSettings, t.context.request), /(.*) by (.*)/i);
+});
