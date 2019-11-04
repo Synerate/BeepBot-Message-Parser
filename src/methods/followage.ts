e39f90461a7196c5f84e1b2ac7b14d7f07dc63c1
Kieron Richardson <artdude543@hotmail.co.uk>
2019-11-04 23:43:08 +0000

    feat(followage): add support for twitch

--- src/methods/followage.ts
+++ src/methods/followage.ts
@@ -6,15 +6,31 @@ import { IMessage } from '../interface';
 import { httpRequest } from '../lib/helpers';
 
 export const methods = {
-    mixer: async (request: typeof fetch, channelId: number, userId: number): Promise<any> => {
+    mixer: async (request: typeof fetch, channelId: number, userId: number): Promise<string> => {
         const req = await httpRequest(request,
                                       `${config.get<string>('providers.mixer.api')}channels/${channelId}/relationship?user=${userId}`,
                                       { 'Client-ID': config.get<string>('providers.mixer.clientId') });
+        if (req == null) {
+            return '[API Error]';
+        }
         if (req.status.follows == null) {
-            return 'User does not follow the channel.';
+            return '[User Not Following]';
         }
 
-        return countdown(new Date(), moment(req.status.follows.createdAt).toDate());
+        return countdown(new Date(), moment(req.status.follows.createdAt).toDate()).toString();
+    },
+    twitch: async (request: typeof fetch, channelId: string, userId: string): Promise<string> => {
+        const req = await httpRequest(request,
+                                      `${config.get<string>('providers.twitch.api')}helix/users/follows?from_id=${userId}&to_id=${channelId}`,
+                                      { 'Client-ID': config.get<string>('providers.twitch.clientId') });
+        if (req == null) {
+            return '[API Error]';
+        }
+        if (req.total === 0 || req.data.length === 0) {
+            return '[User Not Following]';
+        }
+
+        return countdown(new Date(), moment(req.data[0].followed_at).toDate()).toString();
     },
 };
 
--- src/tests/methods/followage.ts
+++ src/tests/methods/followage.ts
@@ -11,21 +11,32 @@ test.beforeEach('create a new mockMessage', (t: any) => {
 });
 
 test('handle a non-follower', async (t: any) => {
-    const message = t.context.message;
-    message.channel.id = 283247;
-    message.user.id = 693;
-    message.provider = 'mixer';
+    const mixer = t.context.message;
+    mixer.channel.id = 283247;
+    mixer.user.id = 693;
+    mixer.provider = 'mixer';
 
-    t.is(await followage(message, undefined, t.context.request), 'User does not follow the channel.');
+    t.is(await followage(mixer, undefined, t.context.request), '[User Not Following]');
+
+    const twitch = t.context.message;
+    twitch.channel.id = 40526358;
+    twitch.user.id = 443665825;
+    twitch.provider = 'twitch';
+    t.is(await followage(twitch, undefined, t.context.request), '[User Not Following]');
 });
 
 test('handle a follower', async (t: any) => {
-    const message = t.context.message;
-    message.channel.id = 3181;
-    message.user.id = 280486;
-    message.provider = 'mixer';
+    const mixer = t.context.message;
+    mixer.channel.id = 3181;
+    mixer.user.id = 280486;
+    mixer.provider = 'mixer';
+    t.not(await followage(mixer, undefined, t.context.request), '[User Not Following]');
 
-    t.not(await followage(message, undefined, t.context.request), 'User does not follow the channel.');
+    const twitch = t.context.message;
+    twitch.channel.id = 40526358;
+    twitch.user.id = 36297622;
+    twitch.provider = 'twitch';
+    t.not(await followage(twitch, undefined, t.context.request), '[User Not Following]');
 });
 
 test('handle a bad-provider', async (t: any) => {
