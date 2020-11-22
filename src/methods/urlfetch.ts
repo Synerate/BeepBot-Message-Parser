8e49f46e9a4fb69cbd38c69a6a7650aaef233eec
Kieron Richardson <artdude543@hotmail.co.uk>
2020-11-22 19:21:51 +0000

    refactor(urlfetch): fix logic for url building

--- src/methods/urlfetch.ts
+++ src/methods/urlfetch.ts
@@ -1,13 +1,18 @@
 import { Parser } from '..';
 import { IMessage } from '../interface';
 
-export async function urlfetch(this: Parser, message: IMessage, _settings: any, _request: typeof fetch, uri: string, pickOpts: string = null) {
-    if (uri == null) {
-        return '[URI Missing]';
-    }
+export async function urlfetch(this: Parser, message: IMessage, _settings: any, _request: typeof fetch, ...uriArgs: string[]) {
     if (this.opts.reqCustomAPI == null) {
         return '[Custom API Not Supported]';
     }
+
+    const copiedArgs = [ ...uriArgs ];
+    copiedArgs.pop();
+    const uri = copiedArgs.join('');
+    if (uri == null) {
+        return '[URI Missing]';
+    }
+    const pickOpts = uriArgs[uriArgs.length - 1];
 
     return this.opts.reqCustomAPI(uri, message, pickOpts)
         .then(res => {
