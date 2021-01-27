b49992f2feac18a7ec8fb01744bac37e77e1a4ed
Kieron Richardson <artdude543@hotmail.co.uk>
2021-01-27 13:49:12 +0000

    fix(urlfetch): simplfy function args
    this resolves an issue when using the data

--- src/methods/urlfetch.ts
+++ src/methods/urlfetch.ts
@@ -1,18 +1,10 @@
 import { Parser } from '..';
 import { IMessage } from '../interface';
 
-export async function urlfetch(this: Parser, message: IMessage, _settings: any, _request: typeof fetch, ...uriArgs: string[]) {
+export async function urlfetch(this: Parser, message: IMessage, _settings: any, _request: typeof fetch, uri: string, pickOpts: string = null) {
     if (this.opts.reqCustomAPI == null) {
         return '[Custom API Not Supported]';
     }
-
-    const copiedArgs = [ ...uriArgs ];
-    copiedArgs.pop();
-    const uri = copiedArgs.join('');
-    if (uri == null) {
-        return '[URI Missing]';
-    }
-    const pickOpts = uriArgs[uriArgs.length - 1];
 
     return this.opts.reqCustomAPI(uri, message, pickOpts)
         .then(res => {
