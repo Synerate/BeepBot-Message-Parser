59edca0ac06e648a885fdd9892efa7eecfd40f8d
Kieron Richardson <artdude543@hotmail.co.uk>
2021-03-02 15:42:48 +0000

    fix(urlfetch): use a prefix on the pickopts
    this is not ideal but resolves an issue with it not working at all
    
    to consider making the tokenizer smarter with arg ordering

--- src/methods/urlfetch.ts
+++ src/methods/urlfetch.ts
@@ -1,9 +1,27 @@
 import { Parser } from '..';
 import { IMessage } from '../interface';
 
-export async function urlfetch(this: Parser, message: IMessage, _settings: any, _request: typeof fetch, uri: string, pickOpts: string = null) {
+export async function urlfetch(this: Parser, message: IMessage, _settings: any, _request: typeof fetch, ...uriArgs: string[]) {
     if (this.opts.reqCustomAPI == null) {
         return '[Custom API Not Supported]';
+    }
+
+    const clonedArgs = [ ...uriArgs ];
+    let uri = '';
+    let pickOpts: string = null;
+
+    for (let i = 0; i < clonedArgs.length; i++) {
+        if (i === (clonedArgs.length - 1) && clonedArgs[i].startsWith('!')) {
+            pickOpts = clonedArgs[i].substring(1);
+
+            continue;
+        }
+
+        uri += clonedArgs[i];
+    }
+
+    if (uri == null || uri.length < 1) {
+        return '[URI Missing]';
     }
 
     return this.opts.reqCustomAPI(uri, message, pickOpts)
