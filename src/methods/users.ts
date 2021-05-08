433e090b72af9e0fc81d0b0e132ef593874f0c78
Kieron Richardson <artdude543@hotmail.co.uk>
2021-05-08 16:41:39 +0100

    fix(user): attempt at fixing issue with the default being used

--- src/methods/users.ts
+++ src/methods/users.ts
@@ -5,7 +5,7 @@ export function user(message: IMessage) {
     return message.user.name;
 }
 
-export function touser(message: IMessage, _settings: ISetting, _request: typeof fetch, noDefault?: any) {
+export function touser(message: IMessage, _settings: ISetting, _request: typeof fetch, noDefault: boolean = false) {
     if (message.message.args[1] !== undefined) {
         if (message.provider === 'discord') {
             return `<@${message.user.id}>`;
@@ -18,7 +18,7 @@ export function touser(message: IMessage, _settings: ISetting, _request: typeof 
         return `<@${message.user.id}>`;
     }
 
-    if (noDefault != null) {
+    if (noDefault) {
         return '';
     }
 
