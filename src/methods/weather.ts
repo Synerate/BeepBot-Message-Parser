9767048aa897b0a47e79c082efadc52171c17f4a
Kieron Richardson <artdude543@hotmail.co.uk>
2021-05-25 12:29:18 +0100

    refactor(weather): return response is more informative now

--- src/methods/weather.ts
+++ src/methods/weather.ts
@@ -1,11 +1,15 @@
 import * as config from 'config';
+import { upperFirst } from 'lodash';
 import { stringify } from 'querystring';
 
 import { IMessage, ISetting } from '../interface';
 import { httpRequest } from '../lib/helpers';
 
 interface IWeather {
-    coord: { lon: number; lat: number };
+    coord: {
+        lon: number;
+        lat: number;
+    };
     weather: {
         id: number;
         main: string;
@@ -13,12 +17,43 @@ interface IWeather {
         icon: string;
     }[];
     base: string;
-    main: { temp: number; pressure: number; humidity: number; temp_min: number; temp_max: number };
+    main: {
+        temp: number;
+        feels_like: number;
+        temp_min: number;
+        temp_max: number;
+        pressure: number;
+        humidity: number;
+        sea_level: number;
+        grnd_level: number;
+    };
     visibility: number;
-    wind: { speed: number; deg: number };
-    clouds: { all: number };
+    wind: {
+        speed: number;
+        deg: number;
+        gust: number;
+    };
+    clouds: {
+        all: number;
+    };
+    rain: {
+        '1h': number;
+        '2h': number;
+    };
+    snow: {
+        '1h': number;
+        '2h': number;
+    };
     dt: number;
-    sys: { type: number; id: number; message: number; country: string; sunrise: number; sunset: number };
+    sys: {
+        type: number;
+        id: number;
+        message: number;
+        country: string;
+        sunrise: number;
+        sunset: number;
+    };
+    timezone: number;
     id: number;
     name: string;
     cod: number;
@@ -26,11 +61,22 @@ interface IWeather {
 
 const directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'];
 
-export function getDirection(deg: number) {
+function getDirection(deg: number) {
     // tslint:disable-next-line:binary-expression-operand-order
     const value = Math.floor(0.5 + (deg / 22.5));
 
     return directions[(value % 16)];
+}
+
+function getTemp(temp: number) {
+    return `${temp.toFixed(2)} C (${Number(temp * 9 / 5 + 32).toFixed(2)} F)`;
+}
+
+function getSpeed(mps: number) {
+    const mph = mps * 2.24;
+    const kph = mph * 1.609344;
+
+    return `${mph.toFixed(2)} mph (${kph.toFixed(2)} kph)`;
 }
 
 export async function weather(_message: IMessage, _settings: ISetting, request: typeof fetch, region: string) {
@@ -44,6 +90,8 @@ export async function weather(_message: IMessage, _settings: ISetting, request: 
         return '[API Error]';
     }
 
-    // TODO: Re-Add the directions.
-    return `${req.name} is expecting ${req.weather[0].description}, with wind speeds of ${req.wind.speed}mph.`;
+    const { main, sys, wind } = req;
+    const currWeather = req.weather[0];
+
+    return `Weather for ${req.name}, ${sys.country}: ${upperFirst(currWeather.description)} with a temperature of ${getTemp(main.temp)}. Wind is blowing from the ${getDirection(wind.deg)} at ${getSpeed(wind.speed)}. Humidity is ${main.humidity}%`;
 }
